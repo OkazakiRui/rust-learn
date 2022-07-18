120a6df6bf75adbe68f967ffcffcb3d7009520f4
OkazakiRui <okazaki200226@gmail.com>
2022-07-19 01:23:07 +0900

    feat: test

--- testing/adder/src/lib.rs
+++ testing/adder/src/lib.rs
@@ -1,8 +1,78 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    String::from("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    // use crate::{add_two, Rectangle};
+
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        // panic!("パニックを起こす");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let _larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let _smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        // assert!(smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "'Carol'は含まれませんでした, value was `{}`",
+            result
+        );
     }
 }
