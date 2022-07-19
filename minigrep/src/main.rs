e4bddac97b1334d3f306cc2714f3c76a635c8df4
OkazakiRui <okazaki200226@gmail.com>
2022-07-19 22:53:24 +0900

    refactor: divided

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::Read;
-use std::{env, process};
+mod libs;
+use libs::Config;
+use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,38 +17,8 @@ fn main() {
     // {}というファイルの中
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = libs::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    // テキストは\n{}です
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // 引数の数が足りません
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
 }
