119215764f5341cca8b3a175576f395d51aed694
OkazakiRui <okazaki200226@gmail.com>
2022-07-19 18:06:22 +0900

    refactor: ロジックを分離

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,25 @@
-use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     // {}を探しています
     println!("Searching for {}", config.query);
     // {}というファイルの中
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
@@ -29,14 +37,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             // 引数の数が足りません
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
