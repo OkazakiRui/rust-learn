0fe1a0a9a0afd3097b60b03c7aaa9ba19e4ef5d0
OkazakiRui <okazaki200226@gmail.com>
2022-07-19 17:47:22 +0900

    feat: config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,18 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
     // {}を探しています
-    println!("Searching for {}", query);
+    println!("Searching for {}", config.query);
     // {}というファイルの中
-    println!("In file {}", filename);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -23,9 +23,14 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
